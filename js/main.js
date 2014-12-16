b8fa04955000c646f24443db9caed8ac2ea60834
Adam Sheaffer <adam.e.sheaffer@gmail.com>
2014-12-16 17:50:43 -0600

    created header, adding photos for carousel

---
+++ js/main.js
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function(){
+//
+//   $('button').click(function(){
+//     var userInput = $('input').val();
+//     console.log(userInput);
+//     var url = 'http://api.seatgeek.com/2/events?per_page=83&venue.name=' + userInput;
+//     $.ajax({
+//       type: "GET",
+//       dataType: "jsonp",
+//       url: url,
+//       success: function(data) {
+//         console.log(data);
+//       },
+//       error: function(err) {
+//         console.log(err);
+//       }
+//     })
+//   });
+// });
